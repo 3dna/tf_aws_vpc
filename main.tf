93396a5273e487f2ca5aa0dc16ad8ee0d2f7cf2e
Dominic Ferrantelli <dominic@ferrantel.li>
2016-09-09 16:27:34 -0700

    made private subnets work

--- main.tf
+++ main.tf
@@ -17,6 +17,20 @@ resource "aws_internet_gateway" "mod" {
   vpc_id = "${aws_vpc.mod.id}"
 }
 
+resource "aws_eip" "nat" {
+  provider = "aws.vpc"
+  vpc = true
+  count = "${length(compact(split(",", var.private_subnets)))}"
+}
+
+resource "aws_nat_gateway" "mod" {
+  provider = "aws.vpc"
+  count = "${length(compact(split(",", var.public_subnets)))}"
+  allocation_id = "${element(aws_eip.nat.*.id, count.index)}"
+  subnet_id = "${element(aws_subnet.public.*.id, count.index)}"
+  depends_on = ["aws_internet_gateway.mod"]
+}
+
 resource "aws_route_table" "public" {
   provider = "aws.vpc"
   vpc_id = "${aws_vpc.mod.id}"
@@ -25,14 +39,23 @@ resource "aws_route_table" "public" {
 
 resource "aws_route" "public_internet_gateway" {
   provider = "aws.vpc"
-    route_table_id = "${aws_route_table.public.id}"
-    destination_cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.mod.id}"
+  route_table_id = "${aws_route_table.public.id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = "${aws_internet_gateway.mod.id}"
+}
+
+resource "aws_route" "private_nat_gateway" {
+  provider = "aws.vpc"
+  count = "${length(compact(split(",", var.private_subnets)))}"
+  route_table_id = "${element(aws_route_table.private.*.id, count.index)}"
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id = "${element(aws_nat_gateway.mod.*.id, count.index)}"
 }
 
 resource "aws_route_table" "private" {
   provider = "aws.vpc"
   vpc_id = "${aws_vpc.mod.id}"
+  count = "${length(compact(split(",", var.private_subnets)))}"
   tags { Name = "${var.name}-private" }
 }
 
@@ -60,7 +83,7 @@ resource "aws_route_table_association" "private" {
   provider = "aws.vpc"
   count = "${length(compact(split(",", var.private_subnets)))}"
   subnet_id = "${element(aws_subnet.private.*.id, count.index)}"
-  route_table_id = "${aws_route_table.private.id}"
+  route_table_id = "${element(aws_route_table.private.*.id, count.index)}"
 }
 
 resource "aws_route_table_association" "public" {
